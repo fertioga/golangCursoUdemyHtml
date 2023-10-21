ba64ca87d58b10d73ea80bd211448d500b9d3a01
fernando.franca <fernando.franca@soluti.com.br>
2023-10-21 20:33:26 -0300

    first commit

---
+++ html.go
@@ -0,0 +1,30 @@
+package html
+
+import (
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+// Titulo recebe um ou mais urls de sites e retorna o título da página
+func Titulo(urls ...string) <-chan string {
+
+	c := make(chan string)
+
+	for _, url := range urls {
+
+		go func(url string) {
+
+			resp, _ := http.Get(url) // http.Get(url) returns a *http.Response
+
+			html, _ := ioutil.ReadAll(resp.Body) // retorna um []byte
+
+			r, _ := regexp.Compile("<title>(.*?)<\\/title>") // retorna um *regexp.Regexp
+
+			c <- r.FindStringSubmatch(string(html))[1] // retorna um []string
+		}(url)
+
+	}
+
+	return c // retorna um channel de strings (o canal não está sendo encerrado)
+}
